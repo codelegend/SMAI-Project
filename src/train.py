efb586462ebcd35f1473b2f124470d16d63fd4fd
Soumya97 <vadlamannati1997@gmail.com>
2017-11-11 18:36:54 +0530

    sdxa

--- src/train.py
+++ src/train.py
@@ -12,16 +12,31 @@ import pickle
 
 import gensim, logging, os, re
 
+WORDS_IN_SENTENCE = 100
+
+def load_class(model, dirname):
+    examples = []
+    for fname in os.listdir(dirname):
+        with open(fname) as f:
+            for line in open(os.path.join(dirname, fname)):
+                sanitised_line = re.sub('<br />|<i/?>|<hr>', '', line
+                for sentence in re.split('[.!?] ', sanitised_line
+                    concat = []
+                    for word in re.split('[,; ]', sentence)
+                        concat.append(model[word])
+                    concat.append()
+                    examples.append(concat)
+    return examples
+
 def load_imdb(file_prefix):
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     model = gensim.models.Word2Vec.load('../models/word2vec')
 
     examples = []
     labels = []
-    with open(file_prefix) as f:
-        for line in open(os.path.join(dirname, fname)):
-            sanitised_line = re.sub('<br />|<i/?>|<hr>', '', line)
-            for sentence in re.split('[.!?] ', sanitised_line):
-                for word in re.split('[,; ]', sentence):
+    pos_examples = load_class('pos')
+    examples.append(pos_examples)
+    neg_examples = load_class('neg')
+    examples.append(pos_examples)
 
 def train():
