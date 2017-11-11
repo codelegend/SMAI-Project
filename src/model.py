f893c7b241b330bd9d215d79adac3672ff9e69a6
Anurudh <anurudh.peduri@research.iiit.ac.in>
2017-11-11 18:05:04 +0530

    .

--- src/model.py
+++ src/model.py
@@ -9,17 +9,18 @@ class SentenceNet(nn.Module):
     def __init__(self, wordvec_len=100, num_filters=10, sentence_len=100):
         super(SentenceNet, self).__init__()
 
-        self.conv1 = nn.Conv1d(1, 100, 3)
+        self.conv1 = nn.Conv2d(1, 100, (3, 128))
         self.pool1 = nn.MaxPool1d(1)
+        self.fc1 = nn.Linear(100, 2)
+        self.dropout1 = nn.Dropout(0.5)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.pool1(x)
+        x = self.fc1(x)
+        x = self.dropout1(x)
         x = F.softmax(x)
         return x
 
     def num_flat_features(self, x):
         return reduce(lambda a, b: a * b, x.size()[1:])
-
-net = SentenceNet()
-print net
