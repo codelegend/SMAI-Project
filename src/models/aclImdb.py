4418c0686114b417ba70ed4616a9bdb1a28540e0
Anurudh <anurudh.peduri@research.iiit.ac.in>
2017-11-24 21:38:54 +0530

    [model:aclImdb] update

--- src/models/aclImdb.py
+++ src/models/aclImdb.py
@@ -1,23 +1,45 @@
+import torch
 from torch import nn
+import torch.nn.functional as F
+from torch.autograd import Variable
 import numpy as np
 
 # **DO NOT CHANGE THE CLASS NAME**
 class Model(nn.Module):
-    def __init__(self, wordvec_dim=100, filter_sizes=[(3, 100), (4, 100), (5, 100)]):
+    def __init__(self, sentence_len=100, wordvec_dim=100,
+        filter_sizes=[(3, 100), (4, 100), (5, 100)]):
         super(Model, self).__init__()
         '''
         Add layers here
         '''
 
-        # Convolution layer
+        # Convolution layer and max pooling
         self.convs = []
+        self.maxpools = []
+        fc_size = 0 # input size to fully connected layer
         for filter_width, num_filters in filter_sizes:
-            self.convs.append(nn.Conv2d(1, num_filters, (filter_width, wordvec_dim))
-        self.convs = nn.ModuleList(self.convs)
+            self.convs.append(
+                # (input_channels, output_channels, (filter_width, wordvec_dim))
+                nn.Conv2d(1, num_filters, (filter_width, wordvec_dim))
+            )
+            self.maxpools.append(
+                nn.MaxPool1d(sentence_len - filter_width + 1)
+            )
+            fc_size += num_filters
 
-        # max pooling
+        self.convs = nn.ModuleList(self.convs)
+        self.maxpools = nn.ModuleList(self.maxpools)
 
         # NN
+        layer_sizes = [fc_size, 100]
+        self.FC1 = nn.Linear(layer_sizes[0], layer_sizes[1])
+        self.dropout1 = nn.Dropout(p = 0.5)
+        self.layer = nn.Linear(layer_sizes[1], 2)
+
+        # random-initialization
+
+    def conv_pool(self, x, conv, pool):
+        return x
 
 
     def forward(self, x):
