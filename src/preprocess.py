86a38b1234368c7a4c035f923caf92bdf7f8982e
Gokul B Nair <gokul.bnair@research.iiit.ac.in>
2017-11-25 04:47:30 +0530

    [parser:aclImdb] Tokenize

--- src/preprocess.py
+++ src/preprocess.py
@@ -18,8 +18,10 @@ def learn_word_vectors(dataset, parser_name, output_dir): # DO NOT EDIT THIS LIN
     sentences = parser.SentenceLoader(dataset_dir='datasets/%s' % dataset, partial_dataset=False)
 
     model = gensim.models.Word2Vec(sentences, min_count=5, size=100, workers=workers)
+    # f=open("temp_output.txt","w")
     # for key in model.wv.vocab:
-    #     print key
+    #     f.write(key+'\n')
+    # f.close()
 
     save_file = 'model_%d.gensim' % get_next_unused_id(output_dir)
     model.save(os.path.join(output_dir, save_file))
